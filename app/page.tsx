4272017ed79dad1f2933d1eadf19eca06de3ef94
Ankit kumar Pandey <83550064+Ankit91153@users.noreply.github.com>
2024-10-02 13:01:24 +0530

    add content dashboard page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,8 @@ export default function Home() {
             AI Content <span className="text-purple-700">Generator</span>
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-            maiores aut iusto, eius dolorem quo accusantium consectetur ad
-            
+           
+          The AI Content Generator creates high-quality written content automatically based on user input and topics provided.            
           </p>
 
           <span>
@@ -33,22 +32,22 @@ export default function Home() {
           <div className="flex flex-col gap-3 border border-slate-100 rounded-lg p-5">
               <div className="bg-[#7E5FF9] w-10 h-10 flex justify-center items-center rounded-lg text-center"><BookTemplate className="text-white"/></div>
               <h4 className="font-bold">25+ Templated</h4>
-              <p className="text-slate-500 italic">Lorem ipsum dolor sit amet. nda andfndf ndfak</p>
+              <p className="text-slate-500 italic">Access over 25 professionally designed content templates.</p>
           </div>
           <div className="flex flex-col gap-3 border border-slate-100 rounded-lg p-5">
               <div className="bg-[#7E5FF9] w-10 h-10 flex justify-center items-center rounded-lg text-center"><ChartColumnStacked className="text-white"/></div>
-              <h4 className="font-bold">25+ Templated</h4>
-              <p className="text-slate-500 italic">Lorem ipsum dolor sit amet. nda andfndf ndfak</p>
+              <h4 className="font-bold">10,000 Words Free</h4>
+              <p className="text-slate-500 italic">Generate up to 10,000 words at no cost.</p>
           </div>
           <div className="flex flex-col gap-3 border border-slate-100 rounded-lg p-5">
               <div className="bg-[#7E5FF9] w-10 h-10 flex justify-center items-center rounded-lg text-center"><BookAudioIcon className="text-white"/></div>
-              <h4 className="font-bold">25+ Templated</h4>
-              <p className="text-slate-500 italic">Lorem ipsum dolor sit amet. nda andfndf ndfak</p>
+              <h4 className="font-bold">Optimize Content</h4>
+              <p className="text-slate-500 italic">Enhance your content for better engagement and SEO.</p>
           </div>
           <div className="flex flex-col gap-3 border border-slate-100 rounded-lg p-5">
               <div className="bg-[#7E5FF9] w-10 h-10 flex justify-center items-center rounded-lg text-center"><ChartColumnBig className="text-white"/></div>
-              <h4 className="font-bold">25+ Templated</h4>
-              <p className="text-slate-500 italic">Lorem ipsum dolor sit amet. nda andfndf ndfak</p>
+              <h4 className="font-bold"> Easy to Use</h4>
+              <p className="text-slate-500 italic">User-friendly interface for seamless content generation.</p>
           </div>
       </div>
     </div>
