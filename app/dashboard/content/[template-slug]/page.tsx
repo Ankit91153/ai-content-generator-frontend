09e0dbd8ae023399733b361f92540b996c0dfdf1
Ankit kumar Pandey <83550064+Ankit91153@users.noreply.github.com>
2024-10-02 15:18:46 +0530

    set layout responsive

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -103,7 +103,7 @@ const Content = (props: PROPS) => {
           Back
         </Button>
       </Link>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-5">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-x-5 py-5">
         {/* FormSection */}
         <FormSection
           selectedTemplate={selectedTemplate}
